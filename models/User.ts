9dc6a51f145bf43758354c66a039db0d4a4dcd6c
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-09-13 15:39:07 +0530

    created User model

---
+++ models/User.ts
@@ -0,0 +1,32 @@
+
+import mongoose, {Schema , models} from 'mongoose';
+import bcrypt from "bcryptjs"
+
+export interface IUser {
+  _id?: mongoose.Types.ObjectId;
+  email:string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+};
+
+const userSchema = new Schema<IUser>({
+  email:{type: String, required: true},
+  password:{type:String, required: true},
+
+},
+{
+  timestamps: true
+});
+
+userSchema.pre("save", async function (next){
+  if(this.isModified('password')){
+    this.password = await bcrypt.hash(this.password, 10)
+  }
+  next();
+})
+
+const User = models?.User || mongoose.model<IUser>("User", userSchema)
+
+export default User
