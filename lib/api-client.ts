24033bd40738fd6c0b0d30ce6cd65f4c7ae0669e
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-09-15 20:16:19 +0530

    Add ApiClient class for video API interactions

---
+++ lib/api-client.ts
@@ -0,0 +1,53 @@
+import { IVideo } from "@/models/Video";
+
+export type VideoFormData = Omit<IVideo, "_id">;
+
+type FetchOptions<T = unknown> = {
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  body?: T;
+  headers?: Record<string, string>;
+};
+
+class ApiClient {
+
+  private async fetch<T>(
+    endPoint: string,
+    options: FetchOptions = {}
+  ):Promise <T>{
+    const {method = "GET", body, headers = {}} = options;
+
+    const defayltHeaders = {
+      "Content-Type" : "Application/json",
+      ...headers,
+    };
+
+    const response = await fetch(`api/${endPoint}`, {
+      method,
+      headers: defayltHeaders,
+      body: body ? JSON.stringify(body): undefined
+    });
+
+    if(!response.ok) {
+      throw new Error(await response.text())
+    }
+    return response.json();
+  }
+
+  async getVideos(){
+    return this.fetch<IVideo[]>("/Videos")
+  }
+
+  async getVideo(id: string){
+    return this.fetch<IVideo[]>(`/Videos${id}`)
+  }
+
+  async createVideo(videoData: VideoFormData){
+    return this.fetch<IVideo>("/videos",{
+      method: "POST",
+      body: videoData
+    })
+  }
+
+}
+
+export const apiClient = new ApiClient();
