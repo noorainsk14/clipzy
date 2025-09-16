1736bab006e9c91f8803512b8fc211eee7f8c7f4
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-09-16 22:26:27 +0530

    Refactor GET video API route for improved error handling and database connection

--- app/api/videos/[id]/route.ts
+++ app/api/videos/[id]/route.ts
@@ -2,25 +2,29 @@ import { connectToDatabase } from "@/lib/db";
 import Video from "@/models/Video";
 import { NextResponse } from "next/server";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const { id } = await  params;
   try {
-    await connectToDatabase();
-
-    const videoId = params.id;  
-
-    const video = await Video.findById({videoId})
-    if(!video ){
-      return NextResponse.json({message: "Video is not found"}, {status: 400})
+    connectToDatabase();
+    const video = await Video.findById(id);
+    if(!video){
+      return NextResponse.json({
+        error:"Video not found"
+      },
+    {
+      status: 400
+    })
     }
-    return NextResponse.json(video)
+     return NextResponse.json(
+      video
+     )
+
+
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return NextResponse.json(
-      {error: "failed to fetch video"},
-      {status: 400}
+      {error: "Something went wrong"},
+      {status: 500}
     )
   }
 }
